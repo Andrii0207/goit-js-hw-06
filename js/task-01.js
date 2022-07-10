f59451b4466b0334158c93b4d815b79cc1beab22
Andrii Popov <popov.andrey80@gmail.com>
2022-07-10 18:50:04 +0300

    finish task-01

--- js/task-01.js
+++ js/task-01.js
@@ -1,11 +1,24 @@
-// const parent = document.querySelector('#categories');
-// const childrenArray = parent.children;
-// console.log(childrenArray);
-// console.log(`Number of categories:`, childrenArray.length);
+////////////////////////////////////
+
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
+
+/////////////////////////////////////
 
 const itemArray = document.querySelectorAll('.item');
 console.log(`Number of categories:`, itemArray.length);
-console.dir(itemArray);
 
-const allInfoEl = itemArray.forEach(item => item.firstElementChild);
-console.log(item);
+const allInfoEl = itemArray.forEach(item => {
+  const nameCateg = item.firstElementChild.textContent;
+  const countChild = item.querySelectorAll('li').length;
+  console.log(`Category:`, nameCateg);
+  console.log(`Elements:`, countChild);
+});
