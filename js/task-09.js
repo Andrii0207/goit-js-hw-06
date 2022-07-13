73d5de0cf9d87ff03aa02a178e2e843ee9344db0
Andrii Popov <popov.andrey80@gmail.com>
2022-07-13 22:38:40 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
+  console.log('это клик по кнопке');
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector('body');
+console.log(body);
+const ChangeColorBtn = document.querySelector('.change-color');
+console.log(ChangeColorBtn);
+const color = document.querySelector('.color');
+console.log(color);
+
+function changeColor() {
+  let newColor = getRandomHexColor();
+  body.style.backgroundColor = newColor;
+  color.textContent = `${newColor}`;
+}
+
+ChangeColorBtn.addEventListener('click', changeColor);
