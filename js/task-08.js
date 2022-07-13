ecdbd2f338b7de70aec3b1fb0a374288d237e0e2
Andrii Popov <popov.andrey80@gmail.com>
2022-07-13 17:21:24 +0300

    add task-08 (finish)

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,28 @@
+const formRegitration = document.querySelector('.login-form');
+console.log(formRegitration);
 
+formRegitration.addEventListener('submit', onFormRegitrationSubmit);
+
+function onFormRegitrationSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+  const emailInput = formElements.email.value;
+  const passwordInput = formElements.password.value;
+
+  if (emailInput === '' || passwordInput === '') {
+    alert('Please fill in all fields of the form!');
+  }
+
+  const formData = {
+    email: emailInput,
+    password: passwordInput,
+  };
+
+  console.dir(formElements);
+  console.log('This is form submit');
+
+  console.log(emailInput);
+  console.log(passwordInput);
+  console.log(formData);
+}
