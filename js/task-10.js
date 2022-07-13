457173f404efb87d01e3df58c0029deb6f3087a6
Andrii Popov <popov.andrey80@gmail.com>
2022-07-14 00:10:04 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const numberEl = document.querySelector('[data-create]');
+console.log(numberEl);
+
+const inputEl = document.querySelector('input');
+console.dir(inputEl.value);
+
+function createBoxes() {
+  console.log(inputEl.value);
+}
